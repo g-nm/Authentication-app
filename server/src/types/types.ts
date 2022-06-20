2e4cc09b4fa5af8f0780506d60e795c1e67ee2b6
g-nm <gmang2236@gmail.com>
2022-06-20 17:46:10 +0300

    server types

---
+++ server/src/types/types.ts
@@ -0,0 +1,28 @@
+declare global {
+  namespace Express {
+    interface User extends IInsertUser {}
+  }
+}
+export interface ISignUp {
+  password: string;
+  email: string;
+  provider?: string;
+}
+export interface IInsertUser extends ISignUp {
+  user_id: string;
+  created_on: string;
+  last_login: string;
+  provider: string;
+}
+export interface IUserDetails {
+  phone: string;
+  bio: string;
+  name: string;
+  picture: string;
+}
+export interface IUpdateUserDetails extends ISignUp, IUserDetails {
+  user_id: string;
+}
+export enum ProviderList {
+  GOOGLE = 'google',
+}

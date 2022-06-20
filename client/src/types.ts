7779583b510fdc78161b928e2c7679ca39cca126
g-nm <gmang2236@gmail.com>
2022-06-20 17:40:44 +0300

    added types

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,6 +1,28 @@
-enum Action {
+const { VITE_TEST: server } = import.meta.env;
+export enum Action {
   SIGNUP = 'signup',
   LOGIN = 'login',
 }
 
-export { Action };
+export interface IUser {
+  email: string;
+  password: string;
+}
+export type AuthContextType = {
+  user: any;
+  signup: (user: IUser, callback: (err: any) => void) => void;
+  login: (user: IUser, callback: (err: any) => void) => void;
+  signout: (callback: VoidFunction) => void;
+};
+
+export interface IUserDetails {
+  phone?: string;
+  bio?: string;
+  name?: string;
+  picture?: string;
+  email: string;
+}
+
+export enum ENV {
+  SERVER = server,
+}

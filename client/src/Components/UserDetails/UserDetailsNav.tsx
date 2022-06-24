a9b804041cf30b5593cbadadc5a7d38668ff6acf
Gnm <gmang2236@gmail.com>
2022-06-24 22:26:22 +0300

    Added sub nav section - User can now sign out - User can navigation to  their profile page

--- client/src/Components/UserDetails/UserDetailsNav.tsx
+++ client/src/Components/UserDetails/UserDetailsNav.tsx
@@ -1,19 +1,83 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 // import useAuth from '../../hooks/useAuth';
 import useMediaMatch from '../../hooks/useMediaMatch';
 import logoDark from '../../assets/devchallenges-light.svg';
 import logo from '../../assets/devchallenges.svg';
 import styles from './UserDetailsNav.module.css';
+import { useGetDetails } from '../../hooks/useGetDetails';
+import avatar from '../../assets/avatar.svg';
+import {
+  MdAccountCircle,
+  MdOutlineArrowDropDown,
+  MdOutlineExitToApp,
+} from 'react-icons/md';
+import useAuth from '../../hooks/useAuth';
+import { useState } from 'react';
 
 const UserDetailsNav = () => {
-  // const { signout } = useAuth();
-  // const navigate = useNavigate();
+  const [showSubNav, setShowSubNav] = useState(false);
+  const { signout } = useAuth();
+  const navigate = useNavigate();
   const isDarkMode = useMediaMatch();
+  const { data } = useGetDetails();
   return (
     <div>
       <nav className={`${styles.nav} container`}>
         <img src={isDarkMode ? logoDark : logo} alt='Dev Challenges logo' />
-        <img src='' alt='' width='32px' height='36px' />
+        <button
+          className={styles.btn__navigation}
+          onClick={() => {
+            setShowSubNav(!showSubNav);
+          }}
+          aria-controls='subNav'
+          aria-expanded={showSubNav}
+        >
+          <img
+            src={data?.picture || avatar}
+            alt='user Avatar'
+            width='32px'
+            height='36px'
+            className={styles.btn__image}
+          />
+          <span className={styles.nav__username}>
+            {data?.name || 'George Mwaniki'}
+          </span>
+          <MdOutlineArrowDropDown />
+        </button>
+        <div
+          className={styles.nav__subNav}
+          style={{ display: showSubNav ? 'block' : 'none' }}
+          id='subNav'
+          tabIndex={-1}
+          onBlur={(e) => {
+            if (!e.currentTarget.contains(e.relatedTarget)) {
+              setShowSubNav(false);
+            }
+          }}
+        >
+          <ul className={styles.subNav__list}>
+            <li className={styles.subNav__item}>
+              <Link to='/details' className={styles.subNav__link}>
+                <MdAccountCircle />
+                <span>My Profile</span>
+              </Link>
+            </li>
+            <li className={styles.subNav__item}>
+              <Link
+                to='/login'
+                onClick={() =>
+                  signout(() => {
+                    navigate('/login', { replace: true });
+                  })
+                }
+                className={styles.subNav__link}
+              >
+                <MdOutlineExitToApp />
+                <span>Signout</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
       <Outlet />
     </div>

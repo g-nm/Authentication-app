62c800fafca97023a59581b5230f48274e5e4303
Gnm <gmang2236@gmail.com>
2022-06-28 15:57:51 +0300

    set env variables

--- server/src/config/postgresdb.ts
+++ server/src/config/postgresdb.ts
@@ -1,11 +1,13 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config({});
 
 const postgresPool = new Pool({
-  user: 'gnm',
-  host: 'localhost',
-  port: 5432,
-  database: 'authdb',
-  password: 'gnm',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT as unknown as number,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   max: 20,
 });
 

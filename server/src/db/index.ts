4c9555df7e4fbeae3ef8a3e725c8617237a39359
Gnm <gmang2236@gmail.com>
2022-06-28 15:59:37 +0300

    set up update details

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,13 +1,16 @@
 import { Profile } from 'passport-google-oauth20';
 import postgresPool from '../config/postgresdb';
-import { ISignUp, ProviderList } from '../types/types';
+import { ISignUp, IUpdateUserDetails, ProviderList } from '../types/types';
 import { InsertUser, InsertUserFromProvider } from './insert';
 import { SelectUserDetails } from './select';
+import { UpdateUserDetails } from './update';
 
 const insertUser = (user: ISignUp) => InsertUser(user, postgresPool);
 const insertUserFromProvider = (profile: Profile, provider: ProviderList) =>
   InsertUserFromProvider(profile, provider, postgresPool);
 const selectUserDetails = (user_id: string) =>
   SelectUserDetails(user_id, postgresPool);
+const UpdateUser = (user: IUpdateUserDetails) =>
+  UpdateUserDetails(postgresPool, user);
 
-export { insertUser, insertUserFromProvider, selectUserDetails };
+export { insertUser, insertUserFromProvider, selectUserDetails, UpdateUser };
--- server/src/db/update.ts
+++ server/src/db/update.ts
@@ -5,50 +5,66 @@ import { hashPassword } from '../scripts/password';
 export const UpdateUserDetails = async (
   pool: Pool,
   userDetails: IUpdateUserDetails
-) => {
+): Promise<IUpdateUserDetails> => {
+  console.log('got called', userDetails);
   const client = await pool.connect();
   try {
     const selectQuery = 'SELECT * FROM user_details where user_id=$1';
     const result = await pool.query(selectQuery, [userDetails.user_id]);
     const user = <IUserDetails[]>result.rows;
+    let userResponse: IUpdateUserDetails = userDetails;
     await client.query('BEGIN');
     if (user.length === 0) {
       const insertNewUserDetails =
-        'INSERT INTO user_details(phone,bio,name,picture) VALUES ($1,$2,$3,$4)';
+        'INSERT INTO user_details(phone,bio,name,picture,user_id) VALUES ($1,$2,$3,$4,$5) RETURNING phone,bio,name,picture';
       const values = [
         userDetails.phone,
         userDetails.bio,
         userDetails.name,
-        userDetails.picture,
-      ];
-      await client.query(insertNewUserDetails, values);
-    } else if (user.length === 1) {
-      const updateUserDetailsQuery =
-        ' UPDATE user_details SET phone=$1,bio=$2,name=$3,picture=$4 WHERE user_id=$5';
-      const updateValues = [
-        userDetails.phone,
-        userDetails.bio,
-        userDetails.name,
-        userDetails.picture,
+        userDetails.picture || '',
         userDetails.user_id,
       ];
-      await client.query(updateUserDetailsQuery, updateValues);
+      const { rows } = await client.query(insertNewUserDetails, values);
+      console.log(rows);
+      userResponse = rows[0];
+    } else if (user.length === 1) {
+      const updateUserDetailsQuery =
+        ' UPDATE user_details SET phone=$1,bio=$2,name=$3,picture=$4 WHERE user_id=$5 RETURNING phone,bio,name,picture';
+      const updateValues = [
+        userDetails.phone || 'phone',
+        userDetails.bio || 'bio',
+        userDetails.name || 'name',
+        userDetails.picture || '',
+        userDetails.user_id,
+      ];
+      const { rows } = await client.query(updateUserDetailsQuery, updateValues);
+      userResponse = rows[0];
     }
 
     const updateUsersQuery =
-      'UPDATE users SET email=$1,password=$2 WHERE user_id=$3';
-    const password = await hashPassword(userDetails.password);
+      'UPDATE users SET email=$1,password=$2 WHERE user_id=$3 RETURNING email';
+    const password = userDetails.password
+      ? await hashPassword(userDetails.password)
+      : 'password';
     const values = [userDetails.email, password, userDetails.user_id];
 
-    await client.query(updateUsersQuery, values);
+    const { rows } = await client.query(updateUsersQuery, values);
+    userResponse = {
+      ...userResponse,
+      ...(rows[0] as Record<'email', string>),
+      password: '',
+      user_id: '',
+    };
+
+    await client.query('COMMIT');
+    return userResponse;
   } catch (error) {
+    console.log('an error occurred');
     await client.query('ROLLBACK');
     throw error;
   } finally {
     client.release();
   }
-
-  return;
 };
 
 export const UpdateUserLoginTime = async (uuid: string, pool: Pool) => {

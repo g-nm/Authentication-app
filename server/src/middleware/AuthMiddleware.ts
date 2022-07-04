606e3c6faa4bcf7464b807e7bd9656a3cade13c6
Gnm <georgenmwaniki@gmail.com>
2022-07-04 16:33:35 +0300

    removed user log

--- server/src/middleware/AuthMiddleware.ts
+++ server/src/middleware/AuthMiddleware.ts
@@ -1,7 +1,6 @@
 import { RequestHandler } from 'express';
 
 export const isRequestAuthenticated: RequestHandler = (req, res, next) => {
-  console.log(req.user);
   if (!req.isAuthenticated()) {
     return res.sendStatus(401);
   }
